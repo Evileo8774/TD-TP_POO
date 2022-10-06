23cde55e40eb12f12e09437d5936ee9dd77465eb
Evileo8774 <debonnetleo4@gmail.com>
2022-10-06 11:46:21 +0200

    tp4 non fini

---
+++ TPs/tp4/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"vlille","l":"Bike"},{"p":"vlille","l":"BikeMain"},{"p":"vlille.util","l":"BikeModel"}]
---
+++ TPs/tp4/src/vlille/BikeStation.java
@@ -0,0 +1,98 @@
+package vlille;
+
+
+public class BikeStation {
+
+   /** array of slots for bikes in the station */
+   private Bike[] bikes;
+   /** name of the station */
+   private String name;
+
+   /**
+    * creates a bike station
+    *
+    * @param name is the name of the bike station
+    * @param capacity is the capacity of bikes of the station
+    */
+   public BikeStation(String name, int capacity) {
+      this.name = name;
+      this.bikes = new Bike[capacity];
+   }
+
+   /**
+    * get the name of the bike station
+    *
+    * @return the bike station name
+    */
+   public String getName() {
+      return name;
+   }
+
+   /**
+    * get the bike capacity of a station
+    *
+    * @return bike capacity of the station
+    */
+   public int getCapacity() {
+      return bikes.length;
+   }
+
+   /**
+    * get the number of bike in a station
+    *
+    * @return the amount of bikes in a station
+    */
+   public int getNumberOfBikes() {
+      int tmp = 0;
+      for(Bike b : bikes){
+         if(b != null) tmp++;
+      }
+      return tmp;
+   }
+
+   /**
+    * get the first free slot in the station
+    *
+    * @return the index of the slot, -1 if the station is full
+    */
+   public int firstFreeSlot() {
+      int freeSlot = -1;
+      for(int i = 0; i < bikes.length; i++){
+         if(bikes[i] == null) {
+            freeSlot = i;
+            break;
+         }
+      }
+      return freeSlot;
+   }
+
+   /**
+    * drop a bike in the station
+    *
+    * @return true if the bike has been drop or false if it couldn't
+    */
+   public boolean dropBike(Bike bike) {
+      int slot = firstFreeSlot();
+      bikes[slot] = bike;
+      if (slot != -1) return true;
+      else return false;
+   }
+
+   /**
+    * Allows to take a bike in the station
+    *
+    * @param i index of the bike in the station
+    *
+    * @return the bike taken
+    */
+   public Bike takeBike(int i) throws BikeNotAvailableException {
+      if(bikes[i] != null){
+         Bike tmp = bikes[i];
+         bikes[i] = null;
+         return tmp;
+      } else {
+         throw new BikeNotAvailableException("Il n'y a pas de vélo à cet emplacement");
+      }
+   }
+
+}
